63717d909442d272227ca1bfdfb8e1d136c8b41b
Manish Yadav <Manish Yadav@LAPTOP-ENE1TNDU>
2022-02-04 12:03:18 +0530

    first commit

---
+++ 4-2-2022/src/com/javaDatatypes/example2.java
@@ -0,0 +1,18 @@
+package com.javaDatatypes;
+
+public class example2 {
+
+	public static void main(String[] args) {
+		/**
+		 * write a java program to print customer information(id,name and address)
+		 */
+		int customerid=1222;
+		String customerName="Samyuktha";
+		String customerAddress="Hyderabad";
+		System.out.println("Customerid");
+		System.out.println("CustomerName");
+		System.out.println("CustomerAddress");
+
+	}
+
+}
