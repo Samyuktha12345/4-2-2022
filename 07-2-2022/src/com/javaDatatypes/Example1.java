f5b19aa195f74e8c195d2c80659c47543564262d
Manish Yadav <Manish Yadav@LAPTOP-ENE1TNDU>
2022-02-07 19:05:38 +0530

    javaDatatypes 07-2-2022

---
+++ 07-2-2022/src/com/javaDatatypes/Example1.java
@@ -0,0 +1,19 @@
+package com.javaDatatypes;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		//int employeeids=387,286,846,675,826;
+		//System.out.println(employeeids[]);
+		
+		int employeeids[]= {387,286,846,675,826};
+		//System.out.println(employeesids[]);
+		
+		for(int employeeid: employeeids)
+		{
+			System.out.println(employeeid);
+		}
+
+	}
+
+}
