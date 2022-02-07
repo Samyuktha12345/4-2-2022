7da2617cf4bec080f12352fc6770f9a5a0f4b439
Manish Yadav <Manish Yadav@LAPTOP-ENE1TNDU>
2022-02-07 11:09:48 +0530

    Second commit

---
+++ 7-02-2022/src/com/javaVairables/Example1.java
@@ -0,0 +1,30 @@
+package com.javaVairables;
+
+public class Example1 {
+	//Write ajava program to print student information using java variables and literals
+	
+	//Variable declaration
+	int studentId;
+	String studentName;
+	float studentMarks;
+	String studentAddress;
+	
+	//Vairable intialization
+	studentId = 1234;
+	studentName = "Samyuktha";
+	studentMarks = 58.45f;
+	studentsAddress = "Hyderabad";
+	
+	System.out.print("student Id:");
+	System.out.println("studentId");
+	
+	System.out.print("student Name:");
+	system.out.println("studentName");
+	
+	System.out.print("student Marks:");
+	System.out.println("studentMarks");
+	
+	System.out.print("student Address:");
+	System.out.println("studentAddress");
+
+}
